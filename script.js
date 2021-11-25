ba4438422a6c099a04f32c823ca94425214e0217
thecptwaffles <65668198+thecptwaffles@users.noreply.github.com>
2021-11-25 09:43:25 -0700

    epic

---
+++ script.js
@@ -0,0 +1,9 @@
+let date = new Date().toISOString().slice(0,10);
+
+let test = new Date().toISOString();
+
+let dateClass = document.getElementsByClassName('date');
+
+console.log(test);
+
+document.getElementById("date").innerHTML = date; 
