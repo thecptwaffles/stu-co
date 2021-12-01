9badfe4f4a8bd9c80b253b5c46567405f109d492
thecptwaffles <65668198+thecptwaffles@users.noreply.github.com>
2021-11-30 17:00:50 -0700

    commiting changes

--- script.js
+++
@@ -1,9 +0,0 @@
-let date = new Date().toISOString().slice(0,10);
-
-let test = new Date().toISOString();
-
-let dateClass = document.getElementsByClassName('date');
-
-console.log(test);
-
-document.getElementById("date").innerHTML = date; 
